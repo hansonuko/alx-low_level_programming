b64f72550c972b452082b59e8f80cb008e745f60
hansonuko <hansonuko@gmail.com>
2022-12-14 11:04:55 -0800

    Print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print signs of a number
+ * @n: numer to b checked for
+ * Return: 1 and print + if n is less than zero
+ * 0 and print 0 if n is 0
+ * -1 and print - if n is less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
