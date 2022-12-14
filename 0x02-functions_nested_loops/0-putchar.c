9ea925f05cf7f1cc1767ff31acdb86b8e7328d89
hansonuko <hansonuko@gmail.com>
2022-12-13 19:59:06 -0800

    Putchar Function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - funtion to print _putchar
+ * is written by Uko
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+return (0);
