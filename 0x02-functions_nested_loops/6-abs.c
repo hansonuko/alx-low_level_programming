0a86aeea33a3147192f0766a9f9e3cac2da32af8
hansonuko <hansonuko@gmail.com>
2022-12-14 18:38:06 -0800

    Absolute integer update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,12 @@
 
 /**
  * _abs - function that computes absolute value of an integer
- * @int: is th int to be checked
+ * @n: is th int to be checked
+ *
  * Return:the abs of the int.
  */
 
-int _abs(int)
+int _abs(int n)
 {
 	if (n >= 0)
 		return (n);
