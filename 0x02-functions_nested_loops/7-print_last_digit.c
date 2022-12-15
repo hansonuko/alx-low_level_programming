2dec86673e325fcf584a52de682e874e2b431409
hansonuko <hansonuko@gmail.com>
2022-12-14 19:00:25 -0800

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print las digit of a number
+ * @int: the number to be checked
+ * Return: value of the digit
+ */
+
+int print_last_digit(int)
+{
+	int last_digit = 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
