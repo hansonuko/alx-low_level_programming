a6c633dc43ebf5cf174756a1905f86f0a5a42ca4
hansonuko <hansonuko@gmail.com>
2022-12-16 02:16:53 -0800

    Is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - prints letters in uppercase
+ * @c: the letter to look out for
+ *
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c <= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
