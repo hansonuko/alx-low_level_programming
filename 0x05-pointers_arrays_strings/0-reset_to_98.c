337ebcf7fbcc47b2ca4e37a27ca00408ecd6bbbd
hansonuko <hansonuko@gmail.com>
2022-12-19 10:08:27 -0800

    reset 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes the pointer and returna the value
+ * @n: int to be checked
+ *
+ * Return: 0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
