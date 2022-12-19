a95410360430cd887ebc7df0fd853c90ffb2d595
hansonuko <hansonuko@gmail.com>
2022-12-19 10:10:48 -0800

    update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ * starting from 1 and 2 followed by new line
+ * Separated by commas and followed by space
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
