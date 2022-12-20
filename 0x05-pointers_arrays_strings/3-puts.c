8b6fd5d76c8a7a006a973483bd56ef6986b0fea2
hansonuko <hansonuko@gmail.com>
2022-12-19 18:33:24 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - writes a string followed by new line
+ * @str: string character to be checked
+ *
+ * Return: Always 0 is success
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
