82f5dea13be0bfca01c78956177d8b5609ca1e7e
hansonuko <hansonuko@gmail.com>
2022-12-20 02:41:47 -0800

    lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter--)
+		putchar(letter);
+	putchar('\n');
+}
