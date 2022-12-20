af1878d0e65de046bebd59e89863496d12461a05
hansonuko <hansonuko@gmail.com>
2022-12-20 01:34:46 -0800

    Base 10 digit print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digits of base 10
+ * end with a new line
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf('\n');
+	return (0);
+}
