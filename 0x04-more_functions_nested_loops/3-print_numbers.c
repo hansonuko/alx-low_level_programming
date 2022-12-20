cfd82aa49cbbb12165d579c5b52e44c6e69486ac
hansonuko <hansonuko@gmail.com>
2022-12-20 05:30:13 -0800

    print numbers task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,10 @@ void print_numbers(void)
 {
 	char i;
 
-	for (i = '0;' i <= '9;'i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-	putchar(i);
+	_putchar(i);
 	}
-	putchar('\n');
+	_putchar('\n');
+	return (0);
 }
