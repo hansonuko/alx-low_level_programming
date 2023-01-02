6b200b9c247e58609ee6a1251a22043284b8147c
hansonuko <hansonuko@gmail.com>
2023-01-01 19:53:03 -0800

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,14 +11,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, c, tmp;
+	int i, j, tmp;
 
-	c = n - 1;
+	j = n - 1;
 
 	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
-		a[i] = a[c];
-		a[c--] = temp;
+		a[i] = a[j];
+		a[j--] = tmp;
 	}
 }
