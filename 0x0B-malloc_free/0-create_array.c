d465086804dd12f950c46655c1469f9b89438ab0
hansonuko <hansonuko@gmail.com>
2023-01-10 05:38:16 -0800

    update to task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 
 	array = (char *)malloc(sizeof(char) * size);
 	if (array == NULL)
-		return(NULL);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
